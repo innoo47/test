0fe9ba70b001f9f933f4e7e714699c1ef85b4f06
Inu <i0n4h0o7@gmail.com>
2024-03-18 19:52:07 +0900

    [백준 코딩테스트] (24.03.18) 10926 ??!

--- test/Level1/main.swift
+++ test/Level1/main.swift
@@ -11,10 +11,17 @@
 
 // MARK: - [ 10869 ] 사칙연산
 
+//import Foundation
+//
+//let n = readLine()!.components(separatedBy: [" "]).map { Int($0)! }
+//let (a, b) = (n[0], n[1])
+//
+//print("\(a + b)\n\(a - b)\n\(a * b)\n\(a / b)\n\(a % b)")
+
+// MARK: - [ 10926 ] ??!
+
 import Foundation
 
-let n = readLine()!.components(separatedBy: [" "]).map { Int($0)! }
-let (a, b) = (n[0], n[1])
-
-print("\(a + b)\n\(a - b)\n\(a * b)\n\(a / b)\n\(a % b)")
+let id = readLine()!
+print("\(id)??!")
 
