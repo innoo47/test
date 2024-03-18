48ac40776c659adbf960fa468077e4f086c011ff
Inu <i0n4h0o7@gmail.com>
2024-03-18 18:50:37 +0900

    [백준 코딩테스트] (24.03.18) 10869 사칙연산

---
+++ test/Level1/main.swift
@@ -0,0 +1,20 @@
+// 백준 코딩테스트 Level 1
+
+// MARK: - [ 1008 ] A/B
+
+//import Foundation
+//
+//let n = readLine()!.components(separatedBy: [" "]).map { Int($0)! }
+//let (a, b) = (n[0], n[1])
+//
+//print(Double(a) / Double(b))
+
+// MARK: - [ 10869 ] 사칙연산
+
+import Foundation
+
+let n = readLine()!.components(separatedBy: [" "]).map { Int($0)! }
+let (a, b) = (n[0], n[1])
+
+print("\(a + b)\n\(a - b)\n\(a * b)\n\(a / b)\n\(a % b)")
+
