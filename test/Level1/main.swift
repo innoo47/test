046147fbe1ef7e428f63be22e149b4bf2567c492
Inu <i0n4h0o7@gmail.com>
2024-03-19 15:08:11 +0900

    [백준 코딩테스트] (24.03.19) 2588 곱셈

--- test/Level1/main.swift
+++ test/Level1/main.swift
@@ -35,12 +35,28 @@
 
 // MARK: - [ 10430 ] 나머지
 
-import Foundation
+//import Foundation
+//
+//let n = readLine()!.components(separatedBy: [" "]).map { Int($0)! }
+//let (a, b, c) = (n[0], n[1], n[2])
+//
+//print((a+b)%c)
+//print(((a%c)+(b%c))%c)
+//print((a*b)%c)
+//print(((a%c)*(b%c))%c)
 
-let n = readLine()!.components(separatedBy: [" "]).map { Int($0)! }
-let (a, b, c) = (n[0], n[1], n[2])
+// MARK: - [ 2588 ] 곱셈
 
-print((a+b)%c)
-print(((a%c)+(b%c))%c)
-print((a*b)%c)
-print(((a%c)*(b%c))%c)
+let input1 = Int(readLine()!)!
+let input2 = Int(readLine()!)!
+
+var a: Int = (input1 * (input2 % 10))
+var b: Int = (input1 * ((input2 % 100) / 10))
+var c: Int = (input1 * (input2 / 100))
+var d: Int = (input1 * input2)
+
+print(a)
+print(b)
+print(c)
+print(d)
+
