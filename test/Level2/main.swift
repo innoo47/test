cee931de54b6f10cb1662efbe1af317695be737b
Inu <i0n4h0o7@gmail.com>
2024-03-19 15:54:20 +0900

    [백준 코딩테스트] (24.03.19) 9498 시험 성적

--- test/Level2/main.swift
+++ test/Level2/main.swift
@@ -2,15 +2,31 @@
 
 // MARK: - [ 1330 ] 두 수 비교하기
 
-import Foundation
+//import Foundation
+//
+//let n = readLine()!.components(separatedBy: [" "]).map { Int($0)! }
+//let (a, b) = (n[0], n[1])
+//
+//if a > b {
+//    print(">")
+//} else if a < b {
+//    print("<")
+//} else if a == b {
+//    print("==")
+//}
 
-let n = readLine()!.components(separatedBy: [" "]).map { Int($0)! }
-let (a, b) = (n[0], n[1])
+// MARK: - [ 9498 ] 시험 성적
 
-if a > b {
-    print(">")
-} else if a < b {
-    print("<")
-} else if a == b {
-    print("==")
+let n = Int(readLine()!)!
+
+if n >= 90 && n <= 100 {
+    print("A")
+} else if n >= 80 && n <= 89 {
+    print("B")
+} else if n >= 70 && n <= 79 {
+    print("C")
+} else if n >= 60 && n <= 69 {
+    print("D")
+} else {
+    print("F")
 }
